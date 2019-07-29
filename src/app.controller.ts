3eb70c99abbbd97b61e0e1489c0c6edcb004416a
lukasg18 <lukas.gomes2010@gmail.com>
2019-07-29 18:53:31 -0300

    chore(auth): auth controller started

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './cmpaas/auth/auth.service';
 
 @Controller('v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly authService: AuthService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('app/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
   }
 }
